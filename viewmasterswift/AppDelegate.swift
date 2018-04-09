232f675e18d9d51a35db848bffe8c05e3cebd648
Sergio R Martin <xsra@borg.local>
2018-04-09 15:50:57 -0400

    Changed Starting screen

--- viewmasterswift/AppDelegate.swift
+++ viewmasterswift/AppDelegate.swift
@@ -31,7 +31,7 @@ class AppDelegate: NSObject, UIApplicationDelegate {
 
     var window: UIWindow?
 
-    
+    /*
 //    private func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
@@ -100,7 +100,7 @@ class AppDelegate: NSObject, UIApplicationDelegate {
         
         return true
     }
-
+*/
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
